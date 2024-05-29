f6117eb5f19e8db9bd38c75a6fe66cc5bef7015c
ArthurViniciusL <arthurviniciussl.contato@gmail.com>
2024-05-29 17:43:24 -0300

    Component Home

--- eve-app/src/App.jsx
+++ eve-app/src/App.jsx
@@ -1,10 +1,9 @@
-import { Component } from "react";
+import { Home } from "./views/Home";
 
-export class App extends Component {
-    render() {
-        return <>
-            <h1>Olá!!!</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa natus deserunt explicabo amet dignissimos sapiente, minus accusamus saepe eveniet molestias eaque commodi cupiditate voluptatibus corporis tempore consectetur incidunt odio. Labore.</p>
-        </>
-    }
+
+export const App = () => {
+    return (
+        <Home></Home>
+    );
+
 }
---
+++ eve-app/src/views/Home/index.jsx
@@ -0,0 +1,8 @@
+import { Component } from "react";
+
+
+export class Home extends Component {
+    render() {
+        return <h1>Olá - Home</h1>
+    }
+}
